1153f68d4c6fac43d447a839ee4fed828035043b
O4epegb <O4epegb@mail.ru>
2015-07-22 19:25:35 +0300

    js code linted a bit

--- OnePage/source/js/common.js
+++ OnePage/source/js/common.js
@@ -10,52 +10,52 @@ $(document).ready(function () {
 		}
 	});
 
-	//Переход по ссылкам меню, выделение активных ссылок после клика
-	$(".menu ul li a, .dots a").click(function () {
-		var currentPage = $('.active-page');
-		var linkIndex = $(this).parent().index();
-		var sectionSelector = $("main").children();
-		if (linkIndex > currentPage.index()) {
-			sectionSelector.filter(function (index) {
-				return index < linkIndex;
-			}).addClass('top-page')
-		} else if (linkIndex < currentPage.index()) {
-			sectionSelector.filter(function (index) {
-				return index >= linkIndex;
-			}).removeClass('top-page')
-		};
-		currentPage.removeClass('active-page');
-		sectionSelector.eq(linkIndex).addClass('active-page');
-		activeMenuLink();
-		activeNavDot();
-	})
-
 	//Выделение активной ссылки в меню после скролла
 	var activeMenuLink = function () {
-		$(".active-menu-link").removeClass("active-menu-link")
+		$(".active-menu-link").removeClass("active-menu-link");
 		$("main").children().each(function (index) {
 			if ($(this).hasClass("active-page")) {
 				$(".menu").find("li").eq(index).children().addClass("active-menu-link");
 			}
 		});
-	}
-	
+	};
+
 	//Выделение активной точки навигации после скролла
 	$(".dots").first().addClass("active-dot");
 	var activeNavDot = function () {
-		$(".active-dot").removeClass("active-dot")
+		$(".active-dot").removeClass("active-dot");
 		$("main").children().each(function (index) {
 			if ($(this).hasClass("active-page")) {
 				$(".dots").eq(index).addClass("active-dot");
 			}
 		});
-	}
+	};
+
+	//Переход по ссылкам меню, выделение активных ссылок после клика
+	$(".menu ul li a, .dots a").click(function () {
+		var currentPage = $('.active-page'),
+			linkIndex = $(this).parent().index(),
+			sectionSelector = $("main").children();
+		if (linkIndex > currentPage.index()) {
+			sectionSelector.filter(function (index) {
+				return index < linkIndex;
+			}).addClass('top-page');
+		} else if (linkIndex < currentPage.index()) {
+			sectionSelector.filter(function (index) {
+				return index >= linkIndex;
+			}).removeClass('top-page');
+		}
+		currentPage.removeClass('active-page');
+		sectionSelector.eq(linkIndex).addClass('active-page');
+		activeMenuLink();
+		activeNavDot();
+	});
 
 	//Скролл страниц (плюс используем функции выделения активной ссылки и точки)
 	$(document).on('mousewheel keydown mousedown', function (e) {
 		if ((e.deltaY < 0 || e.keyCode == 40 || e.keyCode == 32 || e.keyCode == 34) && e.ctrlKey == false) {
-			var currentPage = $('.active-page');
-			var nextPage = currentPage.next();
+			var currentPage = $('.active-page'),
+				nextPage = currentPage.next();
 			if (nextPage.length != 0) {
 				currentPage.removeClass('active-page').addClass('top-page');
 				nextPage.addClass('active-page');
@@ -64,8 +64,8 @@ $(document).ready(function () {
 			activeNavDot();
 			$(".work-items-big").fadeOut();
 		} else if ((e.deltaY > 0 || e.keyCode == 38 || e.keyCode == 33) && e.ctrlKey == false) {
-			var currentPage = $('.active-page');
-			var nextPage = currentPage.prev();
+			var currentPage = $('.active-page'),
+				nextPage = currentPage.prev();
 			if (nextPage.length != 0) {
 				currentPage.removeClass('active-page');
 				nextPage.removeClass('top-page').addClass('active-page');
@@ -77,47 +77,46 @@ $(document).ready(function () {
 			e.preventDefault();
 		}
 	});
-	
+
 	//Показ большой превью из work, парсинг адреса картинки и текста
-	$(".work-item").click(function() {
-		var itemIndex = $(this).index();
-		var imgSrc = $(this).find("img").attr("src");
-		var text = $(this).find("p").text();
+	$(".work-item").click(function () {
+		var itemIndex = $(this).index(),
+			imgSrc = $(this).find("img").attr("src"),
+			text = $(this).find("p").text();
 		$(".work-item-preview").find("img").attr("src", imgSrc);
 		$(".work-item-preview").find("p").text(text);
 		$(".work-items-big").fadeIn();
-	})
-	
+	});
+
 	//Закрытие превью по клику вне превью
 	$(".work-items-big").on('click', function (e) {
-		if ($(e.target).is($('.work-item-preview').find("*")) || $(e.target).is($('.work-item-preview'))) {
-		} else {
+		if ($(e.target).is($('.work-item-preview').find("*")) || $(e.target).is($('.work-item-preview'))) {} else {
 			$(".work-items-big").fadeOut();
 		}
 	});
-	
+
 	//Фильтр work
-	$(".work-filter").children().click(function(){
+	$(".work-filter").children().click(function () {
 		var whichClass = $(this).text().toLowerCase();
 		if (whichClass == "all") {
-			$(".work-item").each(function(){
+			$(".work-item").each(function () {
 				$(this).removeClass("preview-hidden");
 			});
 			$(".work-filter").children().addClass("filter-bg");
 		} else {
-		$(".work-item").not("."+whichClass).addClass("preview-hidden");
-		$("."+whichClass).removeClass("preview-hidden");
-		$(".filter-bg").removeClass("filter-bg");
-		$(this).addClass("filter-bg");
+			$(".work-item").not("." + whichClass).addClass("preview-hidden");
+			$("." + whichClass).removeClass("preview-hidden");
+			$(".filter-bg").removeClass("filter-bg");
+			$(this).addClass("filter-bg");
 		}
 	});
-	
+
 	//Случайные цвета
 	var rndNumber = function () {
-		return Math.floor((Math.random() * colorsArr.length))
-	};
-	var colorsArr = ["bg-blue", "bg-yellow", "bg-pink", "bg-green", "bg-white"];
-	var color;
+		return Math.floor((Math.random() * colorsArr.length));
+	},
+		colorsArr = ["bg-blue", "bg-yellow", "bg-pink", "bg-green", "bg-white"],
+		color;
 
 	$(".apply-btn, .submit-btn").mouseenter(function () {
 		color = rndNumber();
