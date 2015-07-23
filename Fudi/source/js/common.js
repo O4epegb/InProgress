67a3bc90049a295b85e361e8d3955179f1ddbaef
O4epegb <O4epegb@mail.ru>
2015-07-23 23:46:09 +0300

    some js linting

--- Fudi/source/js/common.js
+++ Fudi/source/js/common.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-	
+
 	//Menu-scrolling
 	$(".menu-reasons").click(function () {
 		$(window).stop().scrollTo($(".reasons"), 500);
@@ -10,7 +10,7 @@ $(document).ready(function () {
 	$(".menu-recipes").click(function () {
 		$(window).stop().scrollTo($(".recipes"), 500);
 	});
-	
+
 	//Back-to-top-button
 	var offset = 300,
 		offset_opacity = 1200,
@@ -18,19 +18,18 @@ $(document).ready(function () {
 		$back_to_top = $('.btn-top');
 
 	//hide or show the "back to top" link
-	$(window).scroll(function(){
-		( $(this).scrollTop() > offset ) ? $back_to_top.addClass('btn-top-visible') : $back_to_top.removeClass('btn-top-visible btn-top-fade-out');
-		if( $(this).scrollTop() > offset_opacity ) { 
+	$(window).scroll(function () {
+		($(this).scrollTop() > offset) ? $back_to_top.addClass('btn-top-visible') : $back_to_top.removeClass('btn-top-visible btn-top-fade-out');
+		if ($(this).scrollTop() > offset_opacity) {
 			$back_to_top.addClass('btn-top-fade-out');
 		}
 	});
 
 	//smooth scroll to top
-	$back_to_top.on('click', function(event){
+	$back_to_top.on('click', function (event) {
 		event.preventDefault();
 		$('body,html').animate({
-			scrollTop: 0 ,
-		 	}, scroll_top_duration
-		);
+			scrollTop: 0
+		}, scroll_top_duration);
 	});
 });
--- Portfolio/source/js/common.js
+++ Portfolio/source/js/common.js
@@ -16,39 +16,46 @@ $(document).ready(function () {
 	});
 
 	// Init controller
-	var controller = new ScrollMagic.Controller({refreshInterval: 200,
+	var controller = new ScrollMagic.Controller({
+		refreshInterval: 200,
 		globalSceneOptions: {
 			triggerHook: .69
 		}
 	});
 
 	var scene1 = new ScrollMagic.Scene({
-			triggerElement: '.projects', duration: $('.projects').height()
+			triggerElement: '.projects',
+			duration: $('.projects').height()
 		})
 		.setClassToggle('.menu li:nth-of-type(1)', 'active')
 		.addTo(controller);
 	var scene12 = new ScrollMagic.Scene({
-			triggerElement: '.projects', duration: $('.projects').height()
+			triggerElement: '.projects',
+			duration: $('.projects').height()
 		})
 		.setClassToggle('.header-wrapper', 'header-border-red')
 		.addTo(controller);
 	var scene2 = new ScrollMagic.Scene({
-			triggerElement: '.skills', duration: $('.skills').height()
+			triggerElement: '.skills',
+			duration: $('.skills').height()
 		})
 		.setClassToggle('.menu li:nth-of-type(2)', 'active')
 		.addTo(controller);
 	var scene22 = new ScrollMagic.Scene({
-			triggerElement: '.skills', duration: $('.skills').height()
+			triggerElement: '.skills',
+			duration: $('.skills').height()
 		})
 		.setClassToggle('.header-wrapper', 'header-border-aqua')
 		.addTo(controller);
 	var scene3 = new ScrollMagic.Scene({
-			triggerElement: '.footer-wrapper', duration: $('.footer-wrapper').height()
+			triggerElement: '.footer-wrapper',
+			duration: $('.footer-wrapper').height()
 		})
 		.setClassToggle('.menu li:nth-of-type(3)', 'active')
 		.addTo(controller);
 	var scene32 = new ScrollMagic.Scene({
-			triggerElement: '.footer-wrapper', duration: $('.footer-wrapper').height()
+			triggerElement: '.footer-wrapper',
+			duration: $('.footer-wrapper').height()
 		})
 		.setClassToggle('.header-wrapper', 'header-border-yellow')
 		.addTo(controller);
